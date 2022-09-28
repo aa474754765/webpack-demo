74aee9a5f01a9f791c183575d383e37927b5cb50
jwang <jwang-v@marchex.com>
2022-09-28 16:57:30 +0800

    部署开发环境

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,26 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: './src/index.js',
     print: './src/print.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
   },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true
   },
+  optimization: {
+    runtimeChunk: 'single',
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: '管理输出',
+      title: 'development',
     }),
   ],
   module: {
